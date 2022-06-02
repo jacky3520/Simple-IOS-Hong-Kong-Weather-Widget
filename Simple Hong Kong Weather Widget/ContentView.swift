6515ec1ff8ba558a57985cbeb185f34ae33975e6
jacky.cheung <jacky.cheung@indzz.com>
2022-06-03 03:40:33 +0800

    Initial Commit

---
+++ Simple Hong Kong Weather Widget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simple Hong Kong Weather Widget
+//
+//  Created by Cheung Chin Hang on 3/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple Hong Kong Weather Widget/Simple_Hong_Kong_Weather_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Hong_Kong_Weather_WidgetApp.swift
+//  Simple Hong Kong Weather Widget
+//
+//  Created by Cheung Chin Hang on 3/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Hong_Kong_Weather_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
